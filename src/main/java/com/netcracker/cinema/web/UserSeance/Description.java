e664aa59fb926e9e5aaafbfd4890ffa9547c430e
olkhovyk <ilya.olk@mail.ru>
2016-11-10 20:56:51 +0200

    Task "Creating a session page layout and booking tickets" completed.

---
+++ src/main/java/com/netcracker/cinema/web/UserSeance/Description.java
@@ -0,0 +1,14 @@
+package com.netcracker.cinema.web.UserSeance;
+
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * Created by Илья on 10.11.2016.
+ */
+public class Description extends VerticalLayout {
+
+    public Description() {
+        //Здесь будет реализовано добавление даты, времени сеанса
+        //а так же стоимости билетов в зависимости от зоны
+    }
+}
---
+++ src/main/java/com/netcracker/cinema/web/UserSeance/TotalPrice.java
@@ -0,0 +1,12 @@
+package com.netcracker.cinema.web.UserSeance;
+
+import com.vaadin.ui.HorizontalLayout;
+
+/**
+ * Created by Илья on 10.11.2016.
+ */
+public class TotalPrice extends HorizontalLayout{
+    public TotalPrice() {
+        //Здесь будет реализовано вычисление суммарной цены всех забронированных билетов
+    }
+}
