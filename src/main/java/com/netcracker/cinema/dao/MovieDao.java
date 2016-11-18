c8f330a052c2f922c3b9703f7bc2cb0ea0bbd1e0
dmtrvld@gmail.com <dima1995>
2016-11-18 13:54:50 +0200

    NC1.2 MovieDAO

---
+++ src/main/java/com/netcracker/cinema/dao/MovieDao.java
@@ -0,0 +1,21 @@
+package com.netcracker.cinema.dao;
+
+import com.netcracker.cinema.model.Movie;
+
+import java.util.List;
+
+/**
+ * Created by dimka on 16.11.2016.
+ */
+public interface MovieDao {
+
+    List<Movie> findAll();
+
+    Movie getById(long id);
+
+    void save(Movie movie);
+
+    void update(Movie movie);
+
+    void delete(Movie movie);
+}
