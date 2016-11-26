d16e8fe21e169dfafad78a6a98a03abe344b1cfb
aogim <aogim.h8@gmail.com>
2016-11-26 14:20:39 +0200

    NC1.8 NC1.12
    fix error, when movie was not found

--- src/main/java/com/netcracker/cinema/web/user/MovieDetailsView.java
+++ src/main/java/com/netcracker/cinema/web/user/MovieDetailsView.java
@@ -7,13 +7,16 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.*;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.annotation.PostConstruct;
 
 @SpringView(name = MovieDetailsView.VIEW_NAME, ui = UserUI.class)
 public class MovieDetailsView extends CustomComponent implements View {
     public static final String VIEW_NAME = "details";
+    private static final Logger logger = Logger.getLogger(MovieDetailsView.class);
 
     @Autowired
     private MovieService movieService;
@@ -29,12 +32,21 @@ public class MovieDetailsView extends CustomComponent implements View {
             movieId = Long.parseLong(event.getParameters());
         }
         catch (NumberFormatException e) {
+            logger.info("Expected id, but was " + event.getParameters(), e);
             getUI().getNavigator().navigateTo(MoviesView.VIEW_NAME);
             return;
         }
         //TODO: get all seances for this movie
-        //TODO: catch exception for non existing movie
-        Movie movie = movieService.getById(movieId);
+        Movie movie = null;
+        try {
+            movie = movieService.getById(movieId);
+        }
+        catch (EmptyResultDataAccessException e) {
+            logger.info("Can't find movie with this id " + movieId, e);
+            setCompositionRoot(new Label("Can't find movie with this id " + movieId));
+            return;
+        }
+
         setCompositionRoot(new MovieDetailsComponent(movie, null));
     }
 }
