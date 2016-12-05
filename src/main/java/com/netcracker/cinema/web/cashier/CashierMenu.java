7c4bae26b9f45bca97950450eb80ad9a7f8564ee
dmtrvld@gmail.com <dima1995>
2016-12-05 23:10:57 +0200

    NC 1.13 Added CashierMenu

---
+++ src/main/java/com/netcracker/cinema/web/cashier/CashierMenu.java
@@ -0,0 +1,27 @@
+package com.netcracker.cinema.web.cashier;
+
+import com.netcracker.cinema.web.user.MoviesView;
+import com.netcracker.cinema.web.user.ScheduleView;
+import com.netcracker.cinema.web.user.SeanceView;
+import com.vaadin.ui.MenuBar;
+
+/**
+ * Created by dimka on 04.12.2016.
+ */
+public class CashierMenu extends MenuBar {
+
+    public CashierMenu() {
+        MenuBar.Command command = selectedItem -> {
+            if(selectedItem.getText().equals("Schedule")) {
+//                getUI().getNavigator().navigateTo(ScheduleView.VIEW_NAME);
+            }
+
+            if(selectedItem.getText().equals("Payment confirmation")) {
+                getUI().getNavigator().navigateTo(PaymentView.VIEW_NAME);
+            }
+        };
+
+        addItem("Schedule", command);
+        addItem("Payment confirmation", command);
+    }
+}
