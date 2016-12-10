e2ae2082bcd26cf5742a7366eb319696769a94fa
Titarenko <vitalii.tytarenko@gmail.com>
2016-12-10 19:30:46 +0200

    add Ticket Service

---
+++ src/main/java/com/netcracker/cinema/service/impl/TicketServiceImpl.java
@@ -0,0 +1,56 @@
+package com.netcracker.cinema.service.impl;
+
+import com.netcracker.cinema.dao.TicketDao;
+import com.netcracker.cinema.model.Ticket;
+import com.netcracker.cinema.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Created by Titarenko on 10.12.2016.
+ */
+public class TicketServiceImpl implements TicketService {
+    private TicketDao ticketDao;
+
+    @Autowired
+    TicketServiceImpl(TicketDao ticketDao) {
+        this.ticketDao = ticketDao;
+    }
+
+    @Override
+    public List<Ticket> findAll() {
+        return ticketDao.findAll();
+    }
+
+    @Override
+    public List<Ticket> getBySeanceOrPlace(long id) {
+        return ticketDao.getBySeanceOrPlace(id);
+    }
+
+    @Override
+    public Ticket getById(long ticketId) {
+        return ticketDao.get(ticketId);
+    }
+
+    @Override
+    public void delete(Ticket ticket) {
+        deleteById(ticket.getId());
+    }
+
+    @Override
+    public void deleteById(long ticketId) {
+        ticketDao.delete(ticketId);
+    }
+
+    @Override
+    public void save(Ticket ticket) {
+        ticketDao.save(ticket);
+    }
+
+    @Override
+    public int soldTickets(long objId, Date startDate, Date endDate) {
+        return ticketDao.soldTickets(objId, startDate, endDate);
+    }
+}
