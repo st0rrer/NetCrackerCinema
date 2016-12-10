d0494144d949a6a2ddeeeffaf77ad0d9ab63ad61
Titarenko <vitalii.tytarenko@gmail.com>
2016-12-10 19:29:37 +0200

    update Ticket DAO

--- src/main/java/com/netcracker/cinema/dao/impl/TicketDaoImpl.java
+++ src/main/java/com/netcracker/cinema/dao/impl/TicketDaoImpl.java
@@ -5,14 +5,16 @@ import com.netcracker.cinema.model.Ticket;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.CallableStatementCallback;
+import org.springframework.jdbc.core.CallableStatementCreator;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.*;
-import java.util.Arrays;
-import java.util.List;
+import java.sql.Date;
+import java.util.*;
 
 import static com.netcracker.cinema.dao.impl.queries.TicketDaoQuery.*;
 
@@ -21,6 +23,7 @@ import static com.netcracker.cinema.dao.impl.queries.TicketDaoQuery.*;
  */
 @Repository
 public class TicketDaoImpl implements TicketDao {
+    private List<Ticket> list = Collections.emptyList();
 
     private static final Logger LOGGER = Logger.getLogger(TicketDaoImpl.class);
     private JdbcTemplate jdbcTemplate;
@@ -32,50 +35,112 @@ public class TicketDaoImpl implements TicketDao {
 
     @Override
     public List<Ticket> findAll() {
-        return jdbcTemplate.query(FIND_ALL_TICKETS, new TicketMapper());
+        list = jdbcTemplate.query(FIND_ALL_TICKETS, new TicketMapper());
+        if (list.size() < 1) {
+            LOGGER.info("There are no any tickets in DB");
+        }
+        return list;
     }
 
     @Override
     public List<Ticket> getBySeanceOrPlace(long id) {
-        return jdbcTemplate.query(FIND_TICKET_BY_SEANCE_OR_PLACE, new TicketMapper(), id);
-    }
-
-    @Override
-    public Ticket getById(long id) {
-        Ticket ticket;
-        try {
-            ticket = jdbcTemplate.queryForObject(FIND_TICKET_BY_ID, new TicketMapper(), id);
-        } catch (EmptyResultDataAccessException e) {
-            LOGGER.trace("Id is not exist", e);
-            ticket = null;
+        if (id > 0) {
+            list = jdbcTemplate.query(FIND_TICKET_BY_SEANCE_OR_PLACE, new TicketMapper(), id);
         }
-        return ticket;
+        if (list.size() < 1) {
+            LOGGER.info("There are no tickets for seance or place with id = " + id);
+        } else {
+            LOGGER.info("There are + " + list.size() + " tickets for id = " + id);
+        }
+        return list;
     }
 
     @Override
-    public void deleteById(long ticketId) {
-        LOGGER.trace("Deleting ticket with id = " + ticketId);
-        jdbcTemplate.update(DELETE_TICKET, ticketId);
+    public Ticket get(long ticketId) {
+        if (ticketId < 1) {
+            LOGGER.info("Id should be > 0");
+            return null;
+        } else {
+            Ticket ticket = null;
+            try {
+                ticket = jdbcTemplate.queryForObject(FIND_TICKET_BY_ID, new TicketMapper(), ticketId);
+            } catch (EmptyResultDataAccessException e) {
+                LOGGER.error("Id is not exist for tickets", e);
+            }
+            return ticket;
+        }
     }
 
     @Override
-    public void delete(Ticket ticket) {
-        deleteById(ticket.getId());
+    public void delete(long ticketId) {
+        if (ticketId < 1) {
+            LOGGER.info("Id should be > 0");
+        } else {
+            LOGGER.trace("Deleting ticket with id = " + ticketId);
+            jdbcTemplate.update(DELETE_TICKET, ticketId);
+        }
     }
 
     @Override
     public void save(Ticket ticket) {
-        Object[] array = new Object[]{ticket.getId(), ticket.getCode(), ticket.getEmail(), ticket.getPrice(),
-                (ticket.isPaid() + "").toUpperCase(), ticket.getSeanceId(), ticket.getPlaceId()};
-        LOGGER.trace("Save ticket: " + Arrays.toString(array));
-        jdbcTemplate.update(SAVE_TICKET, array);
+        if (ticket == null) {
+            LOGGER.info("Ticket == null");
+        } else {
+            LOGGER.info("Saving ticket: " + ticket);
+            jdbcTemplate.update(SAVE_TICKET, ticket.getAllFields().toArray());
+        }
+    }
+
+    public void save2(Ticket ticket) {
+        jdbcTemplate.update(MERGE_TICKET_OBJECT, ticket.getId(), ticket.getCode());
+        jdbcTemplate.update(MERGE_TICKET_ATTR_CODE, ticket.getId(), ticket.getCode());
+        jdbcTemplate.update(MERGE_TICKET_ATTR_EMAIL, ticket.getId(), ticket.getEmail());
+        jdbcTemplate.update(MERGE_TICKET_ATTR_PRICE, ticket.getId(), ticket.getPrice());
+        jdbcTemplate.update(MERGE_TICKET_ATTR_PAID, ticket.getId(), (ticket.isPaid() + "").toUpperCase());
+        jdbcTemplate.update(MERGE_TICKET_REF_SEANCE, ticket.getId(), ticket.getSeanceId());
+        jdbcTemplate.update(MERGE_TICKET_REF_PLACE, ticket.getId(), ticket.getPlaceId());
+    }
+
+    public Integer soldTickets2(String objName, Date startDate, Date endDate) {
+        CallableStatementCreator creator = new CallableStatementCreator() {
+            public CallableStatement createCallableStatement(Connection con) {
+                CallableStatement cs = null;
+                try {
+                    cs = con.prepareCall(SOLD_TICKETS_VAR2);
+                    cs.registerOutParameter(1, Types.INTEGER);
+                    cs.setString(2, objName);
+                    cs.setDate(3, startDate);
+                    cs.setDate(4, endDate);
+                } catch (SQLException e) {
+                    LOGGER.trace("Error during create CallableStatement", e);
+                }
+                return cs;
+            }
+        };
+        CallableStatementCallback callback = new CallableStatementCallback() {
+            public Object doInCallableStatement(CallableStatement cs) {
+                int result = 0;
+                try {
+                    cs.execute();
+                    result = cs.getInt(1);
+                } catch (SQLException e) {
+                    LOGGER.trace("Error during execute CallableStatement", e);
+                }
+                return result;
+            }
+        };
+        return (Integer) jdbcTemplate.execute(creator, callback);
     }
 
     @Override
-    public Integer soldTickets(String objName, Date startDate, Date endDate) {
-        List<Integer> list = jdbcTemplate.queryForList(
-                SOLD_TICKETS, Integer.class, objName, startDate, endDate);
-        return list.get(0);
+    public int soldTickets(long objId, Date startDate, Date endDate) {
+        if ((objId < 1) || (startDate.after(endDate))) {
+            LOGGER.info("Id should be > 0");
+            return 0;
+        } else {
+            return jdbcTemplate.queryForObject(
+                    SOLD_TICKETS, int.class, objId, startDate, endDate);
+        }
     }
 
     private class TicketMapper implements RowMapper<Ticket> {
--- src/main/resources/db/function_sold_tickets.sql
+++ src/main/resources/db/function_sold_tickets.sql
@@ -1,68 +1,69 @@
 -- Created by Titarenko on 09.12.2016.
 
 CREATE OR REPLACE FUNCTION Sold_Tickets
-	( obj_name_in IN VARCHAR2
-  ,	start_date_in IN DATE
-  ,	end_date_in IN DATE
-	)
-RETURN INTEGER
+  ( obj_id_in IN VARCHAR2
+    ,	start_date_in IN DATE
+    ,	end_date_in IN DATE
+  )
+  RETURN INTEGER
 IS
   entity OBJTYPE.CODE%TYPE;
-	quantity INTEGER(6);
-BEGIN
-  SELECT OBJTYPE.CODE INTO entity 
+  quantity INTEGER(6);
+  BEGIN
+    SELECT OBJTYPE.CODE INTO entity
     FROM OBJTYPE JOIN OBJECTS obj ON obj.OBJECT_TYPE_ID = OBJTYPE.OBJECT_TYPE_ID
-    WHERE obj.NAME = obj_name_in;
-IF entity = 'ZONE' THEN
-  SELECT COUNT(tick_obj.NAME) INTO quantity
-	FROM OBJTYPE tick_obj_typ
-    JOIN ATTRTYPE tick_atyp    ON tick_atyp.OBJECT_TYPE_ID = tick_obj_typ.OBJECT_TYPE_ID
-    JOIN ATTRIBUTES tick_attr  ON tick_attr.ATTR_ID = tick_atyp.ATTR_ID
-    JOIN OBJECTS tick_obj      ON tick_obj.OBJECT_ID = tick_attr.OBJECT_ID
-    JOIN OBJREFERENCE ref1     ON ref1.REFERENCE = tick_obj.OBJECT_ID
-    JOIN OBJECTS sc_obj        ON sc_obj.OBJECT_ID = ref1.OBJECT_ID
-    JOIN OBJTYPE sc_obj_typ    ON sc_obj_typ.OBJECT_TYPE_ID = sc_obj.OBJECT_TYPE_ID
-    JOIN ATTRIBUTES sc_attr    ON sc_attr.OBJECT_ID = sc_obj.OBJECT_ID
-    JOIN ATTRTYPE sc_atyp      ON sc_atyp.ATTR_ID = sc_attr.ATTR_ID
-    JOIN OBJREFERENCE ref2     ON ref2.REFERENCE = tick_obj.OBJECT_ID
-    JOIN OBJECTS place_obj     ON place_obj.OBJECT_ID = ref2.OBJECT_ID
-    JOIN OBJTYPE place_obj_typ ON place_obj_typ.OBJECT_TYPE_ID = place_obj.OBJECT_TYPE_ID
-    JOIN OBJREFERENCE ref3     ON ref3.REFERENCE = ref2.OBJECT_ID
-    JOIN OBJECTS zone_obj      ON zone_obj.OBJECT_ID = ref3.OBJECT_ID
+    WHERE obj.OBJECT_ID = obj_id_in;
+    IF entity = 'ZONE' THEN
+      SELECT COUNT(tick_obj.NAME) INTO quantity
+      FROM OBJTYPE tick_obj_typ
+        JOIN ATTRTYPE tick_atyp    ON tick_atyp.OBJECT_TYPE_ID = tick_obj_typ.OBJECT_TYPE_ID
+        JOIN ATTRIBUTES tick_attr  ON tick_attr.ATTR_ID = tick_atyp.ATTR_ID
+        JOIN OBJECTS tick_obj      ON tick_obj.OBJECT_ID = tick_attr.OBJECT_ID
+        JOIN OBJREFERENCE ref1     ON ref1.REFERENCE = tick_obj.OBJECT_ID
+        JOIN OBJECTS sc_obj        ON sc_obj.OBJECT_ID = ref1.OBJECT_ID
+        JOIN OBJTYPE sc_obj_typ    ON sc_obj_typ.OBJECT_TYPE_ID = sc_obj.OBJECT_TYPE_ID
+        JOIN ATTRIBUTES sc_attr    ON sc_attr.OBJECT_ID = sc_obj.OBJECT_ID
+        JOIN ATTRTYPE sc_atyp      ON sc_atyp.ATTR_ID = sc_attr.ATTR_ID
+        JOIN OBJREFERENCE ref2     ON ref2.REFERENCE = tick_obj.OBJECT_ID
+        JOIN OBJECTS place_obj     ON place_obj.OBJECT_ID = ref2.OBJECT_ID
+        JOIN OBJTYPE place_obj_typ ON place_obj_typ.OBJECT_TYPE_ID = place_obj.OBJECT_TYPE_ID
+        JOIN OBJREFERENCE ref3     ON ref3.REFERENCE = ref2.OBJECT_ID
+        JOIN OBJECTS zone_obj      ON zone_obj.OBJECT_ID = ref3.OBJECT_ID
       WHERE tick_obj_typ.CODE = 'TICKET'
-        AND tick_atyp.CODE = 'IS_PAID'
-        AND tick_attr.VALUE = 'TRUE'
-        AND sc_obj_typ.CODE = 'SEANCE'
-        AND place_obj_typ.CODE = 'PLACE'
-        AND sc_atyp.CODE = 'DATE'
-        AND sc_attr.DATE_VALUE BETWEEN start_date_in
-                                      AND end_date_in
-        AND zone_obj.NAME = obj_name_in;
-ELSIF entity = 'HALL' OR entity = 'MOVIE' THEN
-  SELECT COUNT(tick_obj.NAME) INTO quantity
-	FROM OBJTYPE tick_obj_typ
-    JOIN ATTRTYPE tick_atyp   ON tick_atyp.OBJECT_TYPE_ID = tick_obj_typ.OBJECT_TYPE_ID
-    JOIN ATTRIBUTES tick_atyp ON tick_atyp.ATTR_ID = tick_atyp.ATTR_ID
-    JOIN OBJECTS tick_obj     ON tick_obj.OBJECT_ID = tick_atyp.OBJECT_ID
-    JOIN OBJREFERENCE ref1    ON ref1.REFERENCE = tick_obj.OBJECT_ID
-    JOIN OBJECTS sc_obj       ON sc_obj.OBJECT_ID = ref1.OBJECT_ID
-    JOIN OBJTYPE sc_obj_typ   ON sc_obj_typ.OBJECT_TYPE_ID = sc_obj.OBJECT_TYPE_ID
-    JOIN ATTRIBUTES sc_attr   ON sc_attr.OBJECT_ID = sc_obj.OBJECT_ID
-    JOIN ATTRTYPE sc_atyp     ON sc_atyp.ATTR_ID = sc_attr.ATTR_ID
-    JOIN OBJREFERENCE ref2    ON ref2.REFERENCE = ref1.OBJECT_ID
-    JOIN OBJECTS hm_obj       ON hm_obj.OBJECT_ID = ref2.OBJECT_ID
-    WHERE tick_obj_typ.CODE = 'TICKET'
-      AND sc_obj_typ.CODE = 'SEANCE'
-      AND tick_atyp.CODE = 'IS_PAID'
-      AND tick_atyp.VALUE = 'TRUE'
-      AND sc_atyp.CODE = 'DATE'
-      AND sc_attr.DATE_VALUE BETWEEN start_date_in
-                               AND end_date_in
-      AND hm_obj.NAME = obj_name_in;
-ELSE
-  RETURN -1;
-END IF;
-	RETURN quantity;
-EXCEPTION WHEN NO_DATA_FOUND THEN
-  RETURN -1;
-END;#
+            AND tick_atyp.CODE = 'IS_PAID'
+            AND tick_attr.VALUE = 'TRUE'
+            AND sc_obj_typ.CODE = 'SEANCE'
+            AND place_obj_typ.CODE = 'PLACE'
+            AND sc_atyp.CODE = 'DATE'
+            AND sc_attr.DATE_VALUE BETWEEN start_date_in
+            AND end_date_in
+            AND zone_obj.OBJECT_ID = obj_id_in;
+    ELSIF entity = 'HALL' OR entity = 'MOVIE' THEN
+      SELECT COUNT(tick_obj.NAME) INTO quantity
+      FROM OBJTYPE tick_obj_typ
+        JOIN ATTRTYPE tick_atyp   ON tick_atyp.OBJECT_TYPE_ID = tick_obj_typ.OBJECT_TYPE_ID
+        JOIN ATTRIBUTES tick_atyp ON tick_atyp.ATTR_ID = tick_atyp.ATTR_ID
+        JOIN OBJECTS tick_obj     ON tick_obj.OBJECT_ID = tick_atyp.OBJECT_ID
+        JOIN OBJREFERENCE ref1    ON ref1.REFERENCE = tick_obj.OBJECT_ID
+        JOIN OBJECTS sc_obj       ON sc_obj.OBJECT_ID = ref1.OBJECT_ID
+        JOIN OBJTYPE sc_obj_typ   ON sc_obj_typ.OBJECT_TYPE_ID = sc_obj.OBJECT_TYPE_ID
+        JOIN ATTRIBUTES sc_attr   ON sc_attr.OBJECT_ID = sc_obj.OBJECT_ID
+        JOIN ATTRTYPE sc_atyp     ON sc_atyp.ATTR_ID = sc_attr.ATTR_ID
+        JOIN OBJREFERENCE ref2    ON ref2.REFERENCE = ref1.OBJECT_ID
+        JOIN OBJECTS hm_obj       ON hm_obj.OBJECT_ID = ref2.OBJECT_ID
+      WHERE tick_obj_typ.CODE = 'TICKET'
+            AND sc_obj_typ.CODE = 'SEANCE'
+            AND tick_atyp.CODE = 'IS_PAID'
+            AND tick_atyp.VALUE = 'TRUE'
+            AND sc_atyp.CODE = 'DATE'
+            AND sc_attr.DATE_VALUE BETWEEN start_date_in
+            AND end_date_in
+            AND hm_obj.OBJECT_ID = obj_id_in;
+    ELSE
+      RETURN -1;
+    END IF;
+      RETURN quantity;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+      RETURN -1;
+  END;
+#
