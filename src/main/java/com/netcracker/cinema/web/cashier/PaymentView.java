026172c00ca7335cc002ad32ccfab3457eedc056
dmtrvld@gmail.com <dima1995>
2016-12-13 16:06:23 +0200

    NC 1.13 Done PaymentView

--- src/main/java/com/netcracker/cinema/web/cashier/PaymentView.java
+++ src/main/java/com/netcracker/cinema/web/cashier/PaymentView.java
@@ -6,6 +6,7 @@ import com.sun.corba.se.spi.ior.ObjectKey;
 import com.vaadin.data.Property;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.event.ShortcutAction;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.BrowserWindowOpener;
@@ -13,9 +14,11 @@ import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.spring.navigator.SpringViewProvider;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -38,13 +41,10 @@ public class PaymentView extends VerticalLayout implements View {
     private Button inputCode;
 
     private VerticalLayout areaForTicketsTable;
-    private Grid ticketsTable;
-    private Button printTickets;
-    private Button sellTickets;
+    @Autowired
+    private TicketsTable ticketsTable;
 
     private Long codeTicket;
-
-    final TestData data = new TestData();;
 
     @PostConstruct
     void init() {
@@ -72,56 +72,23 @@ public class PaymentView extends VerticalLayout implements View {
     }
 
     private void initAreaForTicketsTable() {
-        ticketsTable = new Grid();
 
-        updateList();
+        ticketsTable.setCodeTicket(codeTicket);
+        ticketsTable.updateList();
 
         ticketsTable.setSizeFull();
-        ticketsTable.setColumnOrder("code", "seanceId", "placeId", "price", "email", "paid", "id");
-        ticketsTable.getColumn("id").setHidden(true);
-        ticketsTable.getColumn("seanceId").setHeaderCaption("Seance");
-        ticketsTable.getColumn("placeId").setHeaderCaption("Place");
-
-        ticketsTable.setSelectionMode(Grid.SelectionMode.MULTI);
-
-        HorizontalLayout buttonsForTicketsTable = new HorizontalLayout();
-        sellTickets = new Button("Sell tickets");
-        printTickets = new Button("Print tickets");
-
-        printTickets.setEnabled(false);
-
-        for(Object ticket : ticketsTable.getContainerDataSource().getItemIds()) {
-            if(((Ticket) ticket).isPaid() == true) {
-                printTickets.setEnabled(true);
-                break;
-            }
-        }
-
-        printTickets.addClickListener(event -> {
-            clickPrintTickets();
-        });
-
-        Grid.MultiSelectionModel ticketsSelection  = (Grid.MultiSelectionModel) ticketsTable.getSelectionModel();
-
-        sellTickets.addClickListener(event -> {
-            if(ticketsSelection.getSelectedRows().size() != 0) {
-                getUI().getCurrent().addWindow(new SellWindow(new ArrayList(ticketsSelection.getSelectedRows()), "Confirmation"));
-            }
-        });
-
-        buttonsForTicketsTable.addComponents(sellTickets, printTickets);
-        buttonsForTicketsTable.setSpacing(true);
 
         areaForTicketsTable.setSpacing(true);
         areaForTicketsTable.setMargin(true);
         areaForTicketsTable.setSizeFull();
-        areaForTicketsTable.addComponents(ticketsTable, buttonsForTicketsTable);
+        areaForTicketsTable.addComponents(ticketsTable, ticketsTable.getButtonsForTicketsTable());
         areaForTicketsTable.setComponentAlignment(ticketsTable, Alignment.BOTTOM_CENTER);
 
         addComponent(areaForTicketsTable);
     }
 
     private void initAreaForCode() {
+
         ticketCode = new TextField();
 
         ticketCode.addValueChangeListener(new Property.ValueChangeListener() {
@@ -139,6 +106,7 @@ public class PaymentView extends VerticalLayout implements View {
                     }
                     Long.parseLong(code);
                     getUI().getNavigator().navigateTo(PaymentView.VIEW_NAME + "/" + code);
+
                 } catch (NumberFormatException e) {
                     LOGGER.info("Expected code, but was " + code, e);
                     Notification.show("Ticket code should not have the characters", Notification.Type.ERROR_MESSAGE);
@@ -178,67 +146,9 @@ public class PaymentView extends VerticalLayout implements View {
         addComponent(areaForCode);
     }
 
-    private void clickPrintTickets() {
-        StringBuffer content = new StringBuffer("");
-
-        for(Ticket ticket : (Collection<Ticket>) ticketsTable.getContainerDataSource().getItemIds()) {
-            content.append("<p> " + ticket.getId() + "</p>");
-        }
-
-        content.append(
-                "<SCRIPT language=\"JavaScript\">" +
-                "  print();" +
-                "  close();" +
-                "</SCRIPT>");
-
-        String js = "popup = window.open('', 'printWindow','status=1', 'fullscreen = 1');\n" +
-                "popup.document.write('" + content.toString() + "');\n";
-        JavaScript.getCurrent().execute(js);
-    }
-
     private boolean hasChildComponent(ComponentContainer container) {
         Iterator<Component> iterator = container.iterator();
         if(iterator.hasNext()) return true;
         return false;
     }
-
-    public void updateList() {
-        if(codeTicket != null) {
-            if(!data.getTicketListByCode(codeTicket).isEmpty()) {
-                ticketsTable.setContainerDataSource(new BeanItemContainer<Ticket>(Ticket.class, data.getTicketListByCode(codeTicket)));
-            }
-        }
-    }
-
-    private class SellWindow extends Window {
-        SellWindow(ArrayList ticketsForSell, String caption) {
-            super(caption);
-            VerticalLayout confirmation = new VerticalLayout();
-            confirmation.setMargin(true);
-            confirmation.setSpacing(true);
-            Label confirmationWarring = new Label("The remaining tickets will be reset");
-            Button checkConfirmation = new Button("OK");
-            confirmation.addComponents(confirmationWarring, checkConfirmation);
-            confirmation.setDefaultComponentAlignment(Alignment.BOTTOM_CENTER);
-            this.center();
-            this.setContent(confirmation);
-            checkConfirmation.addClickListener(event -> {
-                List<Ticket> list = data.getTicketListByCode(codeTicket);
-                for(int i = 0; i < list.size(); i++) {
-                    for(int j = 0; j < ticketsForSell.size(); j++) {
-                        if(list.get(i).getId() == ((Ticket) ticketsForSell.get(j)).getId()) {
-                            list.get(i).setPaid(true);
-                        }
-                    }
-                }
-                for(Ticket ticket : list) {
-                    data.save(ticket);
-                }
-                updateList();
-                printTickets.setEnabled(true);
-                getUI().removeWindow(this);
-            });
-        }
-    }
-
 }
---
+++ src/main/java/com/netcracker/cinema/web/cashier/TicketsTable.java
@@ -0,0 +1,204 @@
+package com.netcracker.cinema.web.cashier;
+
+import com.netcracker.cinema.model.*;
+import com.netcracker.cinema.service.*;
+import com.vaadin.data.Item;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.util.GeneratedPropertyContainer;
+import com.vaadin.data.util.PropertyValueGenerator;
+import com.vaadin.data.util.converter.Converter;
+import com.vaadin.server.Resource;
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.*;
+import com.vaadin.ui.renderers.NumberRenderer;
+import com.vaadin.ui.renderers.TextRenderer;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by dimka on 10.12.2016.
+ */
+@SpringComponent
+@UIScope
+@Getter
+@Setter
+public class TicketsTable extends Grid {
+
+    @Autowired
+    private SeanceService seanceService;
+    @Autowired
+    private TicketService ticketService;
+    @Autowired
+    private MovieService movieService;
+    @Autowired
+    private HallService hallService;
+    @Autowired
+    private PlaceService placeService;
+    @Autowired
+    private ZoneService zoneService;
+
+    private Button sellTickets;
+    private Button printTickets;
+    private HorizontalLayout buttonsForTicketsTable;
+
+    private Grid.MultiSelectionModel ticketsSelection;
+
+    private Long codeTicket;
+    private List<Ticket> tickets;
+
+    public TicketsTable() {
+        super();
+        this.setSelectionMode(SelectionMode.MULTI);
+        ticketsSelection  = (Grid.MultiSelectionModel) this.getSelectionModel();
+
+        buttonsForTicketsTable = new HorizontalLayout();
+        sellTickets = new Button("Sell tickets");
+        printTickets = new Button("Print tickets");
+        printTickets.setEnabled(false);
+
+        for(Object ticket : this.getContainerDataSource().getItemIds()) {
+            if(((Ticket) ticket).isPaid() == true) {
+                printTickets.setEnabled(true);
+                break;
+            }
+        }
+
+        printTickets.addClickListener(event -> {
+            clickPrintTickets();
+        });
+
+        sellTickets.addClickListener(event -> {
+            clickSellTickets();
+        });
+
+        buttonsForTicketsTable.addComponents(sellTickets, printTickets);
+        buttonsForTicketsTable.setSpacing(true);
+    }
+
+    private void clickSellTickets() {
+        if(ticketsSelection.getSelectedRows().size() != 0) {
+            getUI().getCurrent().addWindow(new TicketsTable.SellWindow(new ArrayList(ticketsSelection.getSelectedRows()), "Confirmation"));
+        }
+    }
+
+    private void clickPrintTickets() {
+        StringBuffer content = new StringBuffer("");
+
+        for(Ticket ticket : (Collection<Ticket>) this.getContainerDataSource().getItemIds()) {
+            Seance seance = seanceService.getById(ticket.getSeanceId());
+            Movie movie = movieService.getById(seance.getMovieId());
+            Hall hall = hallService.getById(seance.getHallId());
+            Place place = placeService.getById(ticket.getPlaceId());
+            Zone zone = zoneService.getById(place.getZoneId());
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm dd MMM");
+
+            content.append("<p> Code: " + ticket.getCode()  + "</p>");
+            content.append("<p> Seance: " + movie.getName() + " Start Time: " + dateFormat.format(seance.getSeanceDate())
+                    + " Hall: " + hall.getName() + "</p>");
+            content.append("<p> Place. Number: " + place.getNumber() + " Row number: " + place.getRowNumber()
+                    + " Zone: " + zone.getName() + "</p>");
+            content.append("<p> Price: " + ticket.getPrice()  + "</p>");
+            content.append("<br>");
+        }
+
+        content.append(
+                "<SCRIPT language=\"JavaScript\">" +
+                        "  print();" +
+                        "  close();" +
+                        "</SCRIPT>");
+
+        String js = "popup = window.open('', 'printWindow','status=1', 'fullscreen = 1');\n" +
+                "popup.document.write('" + content.toString() + "');\n";
+        JavaScript.getCurrent().execute(js);
+    }
+
+    public void updateList() {
+        if(codeTicket != null) {
+
+            tickets = ticketService.getTicketsByCode(codeTicket);
+            BeanItemContainer<Ticket> ticketsContainer = new BeanItemContainer<>(Ticket.class, tickets);
+
+            GeneratedPropertyContainer ticketsGeneratedContainer = new GeneratedPropertyContainer(ticketsContainer);
+            ticketsGeneratedContainer.addGeneratedProperty("Seance", new PropertyValueGenerator<String>() {
+                @Override
+                public String getValue(Item item, Object itemId, Object propertyId) {
+                    long seanceId = (Long) item.getItemProperty("seanceId").getValue();
+                    Seance seance = seanceService.getById(seanceId);
+                    Movie movie = movieService.getById(seance.getMovieId());
+                    Hall hall = hallService.getById(seance.getHallId());
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm dd MMM");
+                    return movie.getName() + " " + dateFormat.format(seance.getSeanceDate()) + " Hall: " + hall.getName();
+                }
+
+                @Override
+                public Class<String> getType() {
+                    return String.class;
+                }
+            });
+            ticketsGeneratedContainer.addGeneratedProperty("Place", new PropertyValueGenerator<String>() {
+                @Override
+                public String getValue(Item item, Object itemId, Object propertyId) {
+                    long placeId = (Long) item.getItemProperty("placeId").getValue();
+                    Place place = placeService.getById(placeId);
+                    Zone zone = zoneService.getById(place.getZoneId());
+                    return "Number: " + place.getNumber() + " Row: " + place.getRowNumber() + " Zone: " + zone.getName();
+                }
+
+                @Override
+                public Class<String> getType() {
+                    return String.class;
+                }
+            });
+
+            this.setContainerDataSource(ticketsGeneratedContainer);
+            this.setColumnOrder("code", "Seance", "Place", "price", "email", "paid", "id");
+            this.getColumn("id").setHidden(true);
+            this.getColumn("seanceId").setHidden(true);
+            this.getColumn("placeId").setHidden(true);
+        }
+    }
+
+    private class SellWindow extends Window {
+        SellWindow(List ticketsForSell, String caption) {
+            super(caption);
+            VerticalLayout confirmation = new VerticalLayout();
+            confirmation.setMargin(true);
+            confirmation.setSpacing(true);
+            Label confirmationWarring = new Label("The remaining tickets will be reset");
+            Button checkConfirmation = new Button("OK");
+            confirmation.addComponents(confirmationWarring, checkConfirmation);
+            confirmation.setComponentAlignment(checkConfirmation, Alignment.TOP_CENTER);
+            this.center();
+            this.setContent(confirmation);
+            checkConfirmation.addClickListener(event -> {
+                List<Ticket> ticketsByCode = ticketService.getTicketsByCode(codeTicket);
+                for(int i = 0; i < ticketsByCode.size(); i++) {
+                    for(int j = 0; j < ticketsForSell.size(); j++) {
+                        if(ticketsByCode.get(i).getId() == ((Ticket) ticketsForSell.get(j)).getId()) {
+                            ticketsByCode.get(i).setPaid(true);
+                        }
+                    }
+                }
+                for(Ticket ticket : ticketsByCode) {
+                    if(ticket.isPaid() == true) {
+                        ticketService.save(ticket);
+                    } else {
+                        ticketService.deleteById(ticket.getId());
+                    }
+                }
+                updateList();
+                printTickets.setEnabled(true);
+                getUI().removeWindow(this);
+            });
+        }
+    }
+}
