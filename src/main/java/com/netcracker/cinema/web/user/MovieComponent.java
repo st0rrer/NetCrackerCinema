f88527ce5ba23e07df234326fa015268dd5fdfe7
Valentin Karayvansky <Valik-isuzu@mail.ru>
2016-12-19 12:04:53 +0200

    Fix Navigator

--- src/main/java/com/netcracker/cinema/web/user/MovieComponent.java
+++ src/main/java/com/netcracker/cinema/web/user/MovieComponent.java
@@ -10,74 +10,74 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 class MovieComponent extends HorizontalLayout {
-	private Movie movie;
+    private Movie movie;
 
-	public MovieComponent(Movie movie, List<Seance> seances, HallService hallService) {
-		this.movie = movie;
+    public MovieComponent(Movie movie, List<Seance> seances, HallService hallService) {
+        this.movie = movie;
 
-		setSpacing(true);
-		addComponent(createPoster(movie));
-		addComponent(createDetails(movie));
-		addComponent(createSeances(seances, hallService));
-	}
+        setSpacing(true);
+        addComponent(createPoster(movie));
+        addComponent(createDetails(movie));
+        addComponent(createSeances(seances, hallService));
+    }
 
-	public Movie getMovie() {
-		return movie;
-	}
+    public Movie getMovie() {
+        return movie;
+    }
 
-	private Component createSeances(List<Seance> seances, HallService hallService) {
-		VerticalLayout root = new VerticalLayout();
-		root.setSpacing(true);
+    private Component createSeances(List<Seance> seances, HallService hallService) {
+        VerticalLayout root = new VerticalLayout();
+        root.setSpacing(true);
 
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY HH:mm");
-		for(Seance seance: seances) {
-			VerticalLayout seanceInfo = new VerticalLayout();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY HH:mm");
+        for (Seance seance : seances) {
+            VerticalLayout seanceInfo = new VerticalLayout();
 
-			Label date = new Label(dateFormat.format(seance.getSeanceDate()));
-			seanceInfo.addComponent(date);
+            Label date = new Label(dateFormat.format(seance.getSeanceDate()));
+            seanceInfo.addComponent(date);
 
-			Label hall = new Label(hallService.getById(seance.getHallId()).getName());
-			seanceInfo.addComponent(hall);
+            Label hall = new Label(hallService.getById(seance.getHallId()).getName());
+            seanceInfo.addComponent(hall);
 
-			seanceInfo.addLayoutClickListener(event -> getUI().getNavigator()
-					.navigateTo(ScheduleDetailsView.VIEW_NAME + "/" + seance.getId()));
+            seanceInfo.addLayoutClickListener(event -> getUI().getNavigator()
+                    .navigateTo(HallDetailsView.VIEW_NAME + "/" + seance.getId()));
 
-			root.addComponent(seanceInfo);
-		}
+            root.addComponent(seanceInfo);
+        }
 
-		return root;
-	}
+        return root;
+    }
 
-	private Component createDetails(Movie movie) {
-		VerticalLayout root = new VerticalLayout();
-		root.setSpacing(true);
+    private Component createDetails(Movie movie) {
+        VerticalLayout root = new VerticalLayout();
+        root.setSpacing(true);
 
-		Label title = new Label(movie.getName());
-		root.addComponent(title);
+        Label title = new Label(movie.getName());
+        root.addComponent(title);
 
-		Label description = new Label(movie.getDescription());
-		description.setWidth("500px");
-		root.addComponent(description);
+        Label description = new Label(movie.getDescription());
+        description.setWidth("500px");
+        root.addComponent(description);
 
-		Label imdb = new Label("IMDB: " + (double)movie.getImdb() / 10);
-		root.addComponent(imdb);
+        Label imdb = new Label("IMDB: " + (double) movie.getImdb() / 10);
+        root.addComponent(imdb);
 
-		Label duration = new Label("Duration: " + movie.getDuration() + " min");
-		root.addComponent(duration);
+        Label duration = new Label("Duration: " + movie.getDuration() + " min");
+        root.addComponent(duration);
 
-		Label basePrice = new Label("Estimated price: " + movie.getBasePrice());
-		root.addComponent(basePrice);
+        Label basePrice = new Label("Estimated price: " + movie.getBasePrice());
+        root.addComponent(basePrice);
 
-		return root;
-	}
+        return root;
+    }
 
-	private Component createPoster(Movie movie) {
-		ExternalResource res = new ExternalResource(movie.getPoster());
-		Image poster = new Image(null, res);
-		poster.setWidth("200px");
-		poster.setHeight("300px");
-		return poster;
-	}
+    private Component createPoster(Movie movie) {
+        ExternalResource res = new ExternalResource(movie.getPoster());
+        Image poster = new Image(null, res);
+        poster.setWidth("200px");
+        poster.setHeight("300px");
+        return poster;
+    }
 
 
 }
