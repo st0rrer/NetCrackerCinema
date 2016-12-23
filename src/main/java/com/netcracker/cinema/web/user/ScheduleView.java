bb039db65652845710e7abdcad4bf748f339c964
dmtrvld@gmail.com <dima1995>
2016-12-23 15:15:01 +0200

    NC1.15

--- src/main/java/com/netcracker/cinema/web/user/ScheduleView.java
+++ src/main/java/com/netcracker/cinema/web/user/ScheduleView.java
@@ -2,11 +2,14 @@ package com.netcracker.cinema.web.user;
 
 import com.netcracker.cinema.dao.Paginator;
 import com.netcracker.cinema.dao.filter.impl.SeanceFilter;
+import com.netcracker.cinema.model.Hall;
 import com.netcracker.cinema.model.Movie;
 import com.netcracker.cinema.model.Seance;
+import com.netcracker.cinema.service.HallService;
 import com.netcracker.cinema.service.MovieService;
 import com.netcracker.cinema.service.SeanceService;
 import com.netcracker.cinema.web.UserUI;
+import com.vaadin.data.Property;
 import com.vaadin.event.LayoutEvents;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
@@ -14,9 +17,12 @@ import com.vaadin.server.FontAwesome;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @SpringView(name = ScheduleView.VIEW_NAME, ui = UserUI.class)
@@ -28,43 +34,28 @@ public class ScheduleView extends VerticalLayout implements View {
     @Autowired
     SeanceService seanceService;
     @Autowired
-    private ScheduleTable scheduleTable;
+    HallService hallService;
     @Autowired
-    private ScheduleFilterComponent scheduleFilterComponent;
+    private ScheduleTable scheduleTable;
 
-    private SeanceFilter filter = new SeanceFilter().actual().orderByIdDesc();
+    private SeanceFilter seanceFilter = new SeanceFilter().actual().orderByStartDateDesc();
     private Paginator<Seance> paginator;
     private List<Seance> seances;
 
     private PaginationBar paginationBar;
-
+    private ScheduleFilterComponent scheduleFilterComponent;
     @PostConstruct
     void init() {
 
-        paginator = seanceService.getPaginator(SIZE_PAGE, filter);
+        scheduleFilterComponent = new ScheduleFilterComponent();
+        paginationBar = new PaginationBar();
 
         addComponent(scheduleFilterComponent);
         addComponent(scheduleTable);
-
-        scheduleTable.updateGrid(paginator.getPage(1));
-
-        scheduleFilterComponent.getSelectDate().addValueChangeListener(event -> {
-            scheduleFilterComponent.setDateFilter(event.getProperty().getValue());
-            if(!filter.equals(scheduleFilterComponent.getSeanceFilter())) {
-                updatePaginator(scheduleFilterComponent.getSeanceFilter());
-            }
-        });
-
-        scheduleFilterComponent.getSelectHall().addValueChangeListener(event -> {
-            scheduleFilterComponent.setHallFilter(event.getProperty().getValue());
-            if(!filter.equals(scheduleFilterComponent.getSeanceFilter())) {
-                updatePaginator(scheduleFilterComponent.getSeanceFilter());
-            }
-        });
-
-        paginationBar = new PaginationBar(paginator.availablePages());
         addComponent(paginationBar);
         setComponentAlignment(paginationBar, Alignment.TOP_CENTER);
+
+        this.updatePaginator(seanceFilter);
     }
 
     @Override
@@ -72,12 +63,16 @@ public class ScheduleView extends VerticalLayout implements View {
     }
 
     private void updatePaginator(SeanceFilter seanceFilter) {
-        filter = scheduleFilterComponent.getSeanceFilter();
-        paginator = seanceService.getPaginator(SIZE_PAGE, filter);
-        if(paginator.availablePages() >= 0) {
+        paginator = seanceService.getPaginator(SIZE_PAGE, seanceFilter);
+        if (paginator.availablePages() > 1) {
             seances = paginator.getPage(1);
             paginationBar.setMaxPage(paginator.availablePages());
-            scheduleTable.updateGrid(paginator.getPage(1));
+            paginationBar.setVisible(true);
+            scheduleTable.updateGrid(seances);
+        } else if (paginator.availablePages() == 0) {
+            seances = paginator.getPage(1);
+            paginationBar.setVisible(false);
+            scheduleTable.updateGrid(seances);
         }
     }
 
@@ -92,8 +87,7 @@ public class ScheduleView extends VerticalLayout implements View {
         private int currentPage = 1;
         private long maxPage;
 
-        PaginationBar(long maxPage) {
-            this.maxPage = maxPage;
+        PaginationBar() {
             initButtons();
         }
 
@@ -106,8 +100,22 @@ public class ScheduleView extends VerticalLayout implements View {
                     currentPage = (int) maxPage;
                 } else if (event.getButton() == next) {
                     currentPage++;
+                    if(currentPage == maxPage) {
+                        previous.setEnabled(true);
+                        next.setEnabled(false);
+                    } else {
+                        previous.setEnabled(true);
+                        next.setEnabled(true);
+                    }
                 } else if (event.getButton() == previous) {
                     currentPage--;
+                    if(currentPage == 1) {
+                        next.setEnabled(true);
+                        previous.setEnabled(false);
+                    } else {
+                        next.setEnabled(true);
+                        previous.setEnabled(true);
+                    }
                 }
                 scheduleTable.updateGrid(paginator.getPage(currentPage));
                 currentPageView.setValue(String.valueOf(currentPage) + " page");
@@ -138,7 +146,98 @@ public class ScheduleView extends VerticalLayout implements View {
 
         public void setMaxPage(long maxPage) {
             this.maxPage = maxPage;
+            if(currentPage == 1) {
+                previous.setEnabled(false);
+            }
         }
     }
 
+    private class ScheduleFilterComponent extends HorizontalLayout {
+
+        private final String DEFAULT_DATE = "Available seances";
+        private final String DEFAULT_HALL = "All halls";
+
+        private NativeSelect selectDate;
+        private NativeSelect selectHall;
+
+        private Object dateFilter = DEFAULT_DATE;
+        private Object hallFilter = DEFAULT_HALL;
+
+        public ScheduleFilterComponent() {
+            this.setMargin(true);
+            this.setSpacing(true);
+            filterDay();
+            filterHall();
+
+            selectDate.addValueChangeListener(event -> {
+                dateFilter = event.getProperty().getValue();
+                updateSeanceFilter();
+            });
+
+            selectHall.addValueChangeListener(event -> {
+                hallFilter = event.getProperty().getValue();
+                updateSeanceFilter();
+            });
+        }
+
+        private void filterDay() {
+            selectDate = new NativeSelect("Filter by Day");
+            fillScrollOfDate(selectDate);
+            setDefaultNativeSelect(selectDate);
+            this.addComponent(selectDate);
+        }
+
+        private void filterHall() {
+            selectHall = new NativeSelect("Filter by hall");
+            fillScrollOfHalls(selectHall);
+            setDefaultNativeSelect(selectHall);
+            this.addComponent(selectHall);
+        }
+
+        private void setDefaultNativeSelect(NativeSelect nativeSelect) {
+            nativeSelect.setNullSelectionAllowed(false);
+            nativeSelect.setWidth("100px");
+            nativeSelect.setHeight("50px");
+            nativeSelect.setSizeFull();
+            nativeSelect.setImmediate(true);
+        }
+
+        private void fillScrollOfHalls(NativeSelect selectHall) {
+            selectHall.addItem(DEFAULT_HALL);
+            List<Hall> allHalls = hallService.findAll();
+            for(Hall hall : allHalls) {
+                selectHall.addItem(hall);
+                selectHall.setItemCaption(hall, hall.getName());
+            }
+            selectHall.setValue(DEFAULT_HALL);
+        }
+
+        private void fillScrollOfDate(NativeSelect selectDate) {
+            SimpleDateFormat currentDateFormat = new SimpleDateFormat("dd MM yyyy");
+            Date currentDay = new Date();
+            currentDateFormat.format(currentDay);
+            selectDate.addItem(currentDay);
+            selectDate.setItemCaption(currentDay, "Today");
+            selectDate.addItem(DateUtils.addDays(currentDay, 1));
+            selectDate.setItemCaption(DateUtils.addDays(currentDay, 1), "Tomorrow");
+            selectDate.addItem(DateUtils.addDays(currentDay, 7));
+            selectDate.setItemCaption(DateUtils.addDays(currentDay, 7), "Next week");
+            selectDate.addItem(DEFAULT_DATE);
+            selectDate.setValue(DEFAULT_DATE);
+        }
+
+        private void updateSeanceFilter() {
+            seanceFilter = new SeanceFilter().orderByStartDateDesc();
+            if(dateFilter.getClass() == String.class) {
+                seanceFilter.actual();
+            } else {
+                seanceFilter.forDateRange(new Date(), (Date) dateFilter);
+            }
+
+            if(hallFilter.getClass() != String.class) {
+                seanceFilter.forHallId(((Hall) hallFilter).getId());
+            }
+            updatePaginator(seanceFilter);
+        }
+    }
 }
