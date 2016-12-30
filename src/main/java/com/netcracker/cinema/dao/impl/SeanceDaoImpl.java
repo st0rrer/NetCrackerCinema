8276aab912f2b54b7638591bff9743eb468f8337
aogim <aogim.h8@gmail.com>
2016-12-30 15:35:28 +0200

    Add check for input param.

--- src/main/java/com/netcracker/cinema/dao/impl/SeanceDaoImpl.java
+++ src/main/java/com/netcracker/cinema/dao/impl/SeanceDaoImpl.java
@@ -1,21 +1,22 @@
 package com.netcracker.cinema.dao.impl;
 
+import com.netcracker.cinema.dao.Paginator;
 import com.netcracker.cinema.dao.SeanceDao;
 import com.netcracker.cinema.dao.filter.impl.SeanceFilter;
 import com.netcracker.cinema.model.Seance;
-import com.netcracker.cinema.dao.Paginator;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-
-import static com.netcracker.cinema.dao.impl.queries.SeanceDaoQuery.*;
 
 import javax.sql.DataSource;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
+import static com.netcracker.cinema.dao.impl.queries.SeanceDaoQuery.*;
 
 public class SeanceDaoImpl implements SeanceDao {
     private static final Logger logger = Logger.getLogger(SeanceDaoImpl.class);
@@ -36,6 +37,10 @@ public class SeanceDaoImpl implements SeanceDao {
 
     @Override
     public List<Seance> findAll(SeanceFilter filter) {
+        if(filter == null) {
+            throw new IllegalArgumentException("filter can't be null");
+        }
+
         List<Seance> seances = jdbcTemplate.query(filter.buildQuery(), new SeanceRowMapper());
         logger.info("Find all seances with filter: found " + seances.size() + " seance objects");
         return seances;
@@ -43,21 +48,33 @@ public class SeanceDaoImpl implements SeanceDao {
 
     @Override
     public Seance getById(long id) {
-        Seance seance = jdbcTemplate.queryForObject(FIND_SEANCE_BY_ID, new Object[]{id}, new SeanceRowMapper());
-        logger.info("Get seance by id: found not null seance");
+        Seance seance = null;
+        try {
+            seance = jdbcTemplate.queryForObject(FIND_SEANCE_BY_ID, new Object[]{id}, new SeanceRowMapper());
+            logger.info("Get seance by id: found not null seance");
+        } catch (EmptyResultDataAccessException e) { // ok, just 0 sessions
+            logger.info("Can't find seance with id " + id + " return null");
+        }
         return seance;
     }
 
     @Override
     public List<Seance> getByHallAndDate(long hallId, Date date) {
+        if(date == null) {
+            throw new IllegalArgumentException("date can't be null");
+        }
         List<Seance> seances = jdbcTemplate.query(
                 FIND_SEANCE_BY_HALL_AND_DATE, new SeanceRowMapper(), hallId, date);
-        logger.info(seances.size() + " was found for hall_id = " + hallId);
+        logger.info("Get seance by hall and date: found " + seances.size() + " seances for hall " + hallId);
         return seances;
     }
 
     @Override
     public void save(Seance seance) {
+        if(seance == null) {
+            throw new IllegalArgumentException("Can't save null as seance");
+        }
+
         int affected = jdbcTemplate.update(MERGE_SEANCE_OBJECT, seance.getId());
         jdbcTemplate.update(MERGE_SEANCE_ATTRIBUTE_DATE, seance.getSeanceDate(), seance.getId());
         jdbcTemplate.update(MERGE_SEANCE_ATTRIBUTE_MOVIE, seance.getMovieId(), seance.getId());
@@ -72,7 +89,11 @@ public class SeanceDaoImpl implements SeanceDao {
 
     @Override
     public void delete(Seance seance) {
-        int affected = jdbcTemplate.update(DELETE_SEANCE, new Object[]{seance.getId()});
+        if(seance == null) {
+            throw new IllegalArgumentException("Can't delete null seance");
+        }
+
+        int affected = jdbcTemplate.update(DELETE_SEANCE, seance.getId());
         logger.info("Delete seance: affected " + affected + " rows");
     }
 
@@ -83,6 +104,10 @@ public class SeanceDaoImpl implements SeanceDao {
 
     @Override
     public Paginator<Seance> getPaginator(int pageSize, SeanceFilter filter) {
+        if(filter == null) {
+            throw new IllegalArgumentException("Can't return paginator with null filter");
+        }
+
         return new SeancePaginator(pageSize, filter);
     }
 
