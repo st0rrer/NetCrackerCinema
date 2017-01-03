f94a7ee74295a003a25510c56dc0d6dddf58e47c
dmtrvld@gmail.com <dima1995>
2017-01-03 14:46:51 +0200

    Add ApplicationContextHandler

---
+++ src/main/java/com/netcracker/cinema/service/ApplicationContextHandler.java
@@ -0,0 +1,24 @@
+package com.netcracker.cinema.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by dimka on 31.12.2016.
+ */
+@Component
+public class ApplicationContextHandler implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) {
+        context = ctx;
+    }
+}
--- src/main/java/com/netcracker/cinema/web/CinemaConfiguration.java
+++ src/main/java/com/netcracker/cinema/web/CinemaConfiguration.java
@@ -1,9 +1,13 @@
 package com.netcracker.cinema.web;
 
+import com.netcracker.cinema.service.ApplicationContextHandler;
 import com.vaadin.spring.annotation.EnableVaadin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableVaadin
 public class CinemaConfiguration {
+    @Autowired
+    ApplicationContextHandler handler;
 }
